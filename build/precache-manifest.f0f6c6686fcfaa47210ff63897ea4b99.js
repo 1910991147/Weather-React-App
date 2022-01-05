1fb5b5297972f13e9f94c6cd95f02c72b26fd9fa
VINEET VASHIST <68139558+1910991147@users.noreply.github.com>
2022-01-05 16:15:05 +0530

    uploaded files

---
+++ build/precache-manifest.f0f6c6686fcfaa47210ff63897ea4b99.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3cc01ed2e0af82a10216925063829f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef0c32c104403820ac2e",
+    "url": "/static/css/2.09487536.chunk.css"
+  },
+  {
+    "revision": "29c3af2b4e11c73564ac",
+    "url": "/static/css/main.9c444607.chunk.css"
+  },
+  {
+    "revision": "ef0c32c104403820ac2e",
+    "url": "/static/js/2.9b0d9541.chunk.js"
+  },
+  {
+    "revision": "29c3af2b4e11c73564ac",
+    "url": "/static/js/main.8b78555e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08c412ac199fd6ba012276054b5897c5",
+    "url": "/static/media/bg-weather_app.08c412ac.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
